ae7256fe21c3164bba93b69f2294d5c55f453579
LiliSuv <faufy@yandex.ru>
2024-07-12 02:48:50 +0300

    Add files via upload

---
+++ GGthtvtyyst/homework5.py
@@ -0,0 +1,10 @@
+immutable_var_tuple=[20, 'мая'], 2023, ['я', 'выиграла'], [1,2,7], 'рублей'
+print (immutable_var_tuple)
+print( 'immutable_var_tuple[1]=21 действие недопустимое, так как объект "кортеж" не поддерживает назначение элементов')
+immutable_var_tuple [2][1]='проиграла'
+immutable_var_tuple [3][0]=0
+immutable_var_tuple [3][2]=5
+immutable_var_tuple[0][0]=21
+print (immutable_var_tuple)
+
+
